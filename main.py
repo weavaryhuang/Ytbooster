c8e2d2d056f76235ee2b0db58029bd1905645fa5
Henry Huang <weavary@gmail.com>
2021-12-18 12:01:57 +0000

    import

---
+++ main.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+import time
+
+no_of_driver = int(input("Enter number of drivers: "))
+url = input("Enter URL: ")
+time_to_refresh = int(input("Enter refresh rate in seconds: "))
+drivers = []
+
+for i in range(no_of_driver):
+    drivers.append(webdriver.Chrome('Chromedriver\chromedriver.exe'))
+    drivers[i].get(url)
+
+while True:
+    time.sleep(time_to_refresh)
+    for i in range(no_of_driver):
+        drivers[i].refresh()
+
